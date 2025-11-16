7dc2b904d64341debe3ab48a7e7214d7e7223a1c
Gina <64642337+Gina-bun@users.noreply.github.com>
2025-11-16 13:29:05 +0000

    Added code for main.jsx, App.jsx, index.css

---
+++ Cojekk/src/App.jsx
@@ -0,0 +1,24 @@
+
+import { Routes, Route } from 'react-router-dom';
+import { ChatbotInput } from './components/ChatbotInput';
+import { LandingPage } from './pages/LandingPage';
+import { ChatMessages } from './components/ChatMessages';
+import { ProjectGenPage } from './pages/ProjectGenPage';
+
+import './App.css';
+
+
+function App() {
+  return (
+   <Routes>
+    <Route path="/" element={<LandingPage />} />
+  
+    <Route path='/project-gen' element={<ProjectGenPage />} />
+    <Route path='/chatbot-input' element={<ChatbotInput />}></Route>
+     <Route path='/chatbot-messages' element={<ChatMessages  />}></Route>
+
+   </Routes>
+  )
+}
+
+export default App;
